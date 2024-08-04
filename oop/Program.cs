8bd6a7b5cbb28a9e9b2de6ea5f4cdc3e9af9f9ec
paolo flore <paoloflore937@gmail.com>
2024-08-04 18:04:12 +0200

    Aggiungere i file di progetto.

---
+++ oop/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Auto audi = new Auto();
+            audi.Ruote = new Ruote();
+            audi.Ruote.diametro = 10;
+            audi.Ruote.Pressione = 10;
+            audi.Porte = 4;
+            Console.WriteLine(audi.Ruote.diametro);
+
+        }
+    }
+
+    public class Auto
+    {
+        public int Porte {  get; set; }
+        public Ruote Ruote { get; set; }
+
+
+    }
+    public class Ruote
+    {
+        public int Pressione { get; set; }
+        public int diametro { get; set; }
+
+    }
+}
